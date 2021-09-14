c35aa9516146dc5cc8e45f3afb199aa84142bf31
Mary3006 <ms.stasevich@gmail.com>
2021-09-14 19:43:28 +0300

    feat: add logo list

--- web-app/src/components/Logo.js
+++ web-app/src/components/Logo.js
@@ -1,14 +1,34 @@
 function Logo() {
     return(
-        <logo>
-        <ul classname="logo_img">
-        <img src="/logo-atlant.png" classname="logo_atlant" />
-        <img src="/logo-line.png" classname="logo_line"/>
-        <img src="/logo-ford.png" classname="logo_ford"/>
-        </ul>
+        <logo className="logo">
+            <ul className="logo_img">
+                <a href="https://www.borovaya.by/">
+                    <img src="/logo-atlant.png" className="logo_atlant" />
+                </a>
+                    <img src="/logo-line.png" className="logo_line"/>
+                <a href="https://www.borovaya.by/">
+                    <img src="/logo-ford.png" className="logo_ford"/>
+                </a>
+            </ul>
+            <LogoLink/>
         </logo>
     );
 
 }
 
 export default Logo;
+
+function LogoLink() {
+    return(
+        <ul className="logo_list">
+            <a href="https://www.borovaya.by/#model">Автомобили</a>
+            <a href="https://www.borovaya.by/service/">Сервис</a>
+            <a href="https://www.borovaya.by/#spare">Запчасти и аксессуары</a>
+            <a href="https://www.borovaya.by/#repair">Кузовной ремонт</a>
+            <a href="https://www.borovaya.by/finance/lizing-dlya-yuridicheskikh-lits/">Финансовые услуги</a>
+            <a href="https://www.borovaya.by/#online">Клиентам</a>
+        
+        </ul>
+
+    );
+}

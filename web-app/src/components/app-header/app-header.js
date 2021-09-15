c642aad506cc8f013536d7107be7d28cd87913a6
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-09-15 23:48:26 +0300

    feat: add buttons

--- web-app/src/components/app-header/app-header.js
+++ web-app/src/components/app-header/app-header.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable import/no-unresolved */
 import React from "react";
@@ -6,11 +9,24 @@ import React from "react";
 
 export default class AppHeader extends React.Component {
   render() {
+    const { onRequestCall, signService } = this.props;
     return (
       <div className="app-header">
         <h2>Атлант-М Боровая</h2>
         <h2>Минский р-н, р-н д.Боровая, 2</h2>
         <h2>+375 44 550 93 37</h2>
+        <button
+          type="button"
+          className="request-call"
+          value="Заказать звонок"
+          onClick={onRequestCall}
+        />
+        <button
+          type="button"
+          className="sign-service"
+          value="Записаться на сервис"
+          onClick={signService}
+        />
       </div>
     );
   }

82497599926e697c46f9cc3275ba870e9c614a63
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-09-19 01:32:08 +0300

    feat: make component BreadCrumb

--- web-app/src/components/breadcrumb/breadcrumb.js
+++ web-app/src/components/breadcrumb/breadcrumb.js
@@ -1,5 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prefer-stateless-function */
 /**
  * TODOs  добавить material-ui https://material-ui.com/ru/components/breadcrumbs/
  * ? нужна ли интеграция с react-router
 
  */
+import React from "react";
+
+export default class BreadCrumbs extends React.Component {
+  render() {
+    return (
+      <nav className="breadcrumb">
+        <ul>
+          <li>
+            <a href="">Хлебные крошки</a>
+          </li>
+          <li>
+            <a href="">Хлебные крошки</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}

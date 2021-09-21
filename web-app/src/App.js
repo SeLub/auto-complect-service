f11e9888a0f526ffb0971b2f3850d0802f6ae1b7
diamrem <marinasupitsa@gmail.com>
2021-09-21 09:41:33 +0300

    fix:clarity, path

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-// Это корневой компонент
-// Сохраняйте компоненты в папке components
-// Если создаёте универсальный компонет (например кнопка) сохраняйте в папку components/generic
-// Для своих корневых компонентов можно создавать свои подпапки. Например components/products для компонета автомобиля
+import Block_ford_cards from './components/Block_ford_cards';
 
 
 function App() {
   return (
     <div className="App">
+      <Block_ford_cards />
 
     </div>
   );
---
+++ web-app/src/components/Block_ford_cards.js
@@ -0,0 +1,76 @@
+import './Block_ford_cards.css';
+
+import React, { Component } from "react";
+
+
+class Block_ford_cards extends  Component {
+  render(){
+  return (
+    
+    <body class="main-page-body">
+    <div className="block_ford_cards">
+
+       <div className="block_ford_card1">
+      
+       <img src="/img/main_pic1.jpg" className="main_ford_pic" />
+       <div className="minor_ford_pic">  
+       <img src="/img/main_pic101.jpg" className="main_ford_pic101" />
+       <img src="/img/main_pic102.jpg" className="main_ford_pic102 clarity" />
+       <img src="/img/main_pic103.jpg" className="main_ford_pic103 clarity" />   
+       <img src="/img/rectangle.jpg" className="rectangle" />
+       </div>
+       </div>
+
+
+       <div className="block_ford_card2">
+       <img src="/img/main_pic2.jpg" className="main_ford_pic" />
+       <div className="minor_ford_pic">  
+       <img src="/img/main_pic201.jpg" className="main_ford_pic201 clarity" />
+       <img src="/img/main_pic202.jpg" className="main_ford_pic202" />
+       <img src="/img/main_pic203.jpg" className="main_ford_pic203 clarity" />   
+       <img src="/img/rectangle.jpg" className="rectangle" />
+       </div>
+       </div>
+
+
+       <div className="block_ford_card3">
+       <img src="/img/main_pic3.jpg" className="main_ford_pic" />
+       <div className="minor_ford_pic">  
+       <img src="/img/main_pic301.jpg" className="main_ford_pic301" />
+       <img src="/img/main_pic302.jpg" className="main_ford_pic302 clarity" />
+       <img src="/img/main_pic303.jpg" className="main_ford_pic303 clarity" />   
+       <img src="/img/rectangle.jpg" className="rectangle" />
+       </div>
+       </div>
+
+
+       <div className="block_ford_card4">
+       <img src="/img/main_pic4.jpg" className="main_ford_pic" />
+       <div className="minor_ford_pic">  
+       <img src="/img/main_pic401.jpg" className="main_ford_pic401" />
+       <img src="/img/main_pic402.jpg" className="main_ford_pic402 clarity" />
+       <img src="/img/main_pic403.jpg" className="main_ford_pic403 clarity" />   
+       <img src="/img/rectangle.jpg" className="rectangle" />
+       </div>
+       </div>
+
+
+
+       <div className="block_ford_card5">
+       <img src="/img/main_pic5.jpg" className="main_ford_pic" />
+       <div className="minor_ford_pic">  
+       <img src="/img/main_pic501.jpg" className="main_ford_pic501" />
+       <img src="/img/main_pic502.jpg" className="main_ford_pic502 clarity" />
+       <img src="/img/main_pic503.jpg" className="main_ford_pic503 clarity" />   
+       <img src="/img/rectangle.jpg" className="rectangle" />
+       </div>
+       </div>
+      
+      
+      </div>
+    </body>
+  );
+  }
+}
+
+export default Block_ford_cards;

61179f455891a734e65e240696ff4d0e2de5bc63
diamrem <marinasupitsa@gmail.com>
2021-09-22 10:35:14 +0300

    fix:model1

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,8 +5,10 @@ import Block_ford_cards from './components/Block_ford_cards';
 function App() {
   return (
     <div className="App">
+      
+      <div>
       <Block_ford_cards />
-
+      </div>
     </div>
   );
 }

f7f7dbd8b1bb07b18557416de72dcd6000fb6f84
Sergey Lubimov <siteup24@gmail.com>
2021-09-27 14:05:45 +0300

    init: start Frontend mockup

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,14 +1,45 @@
 import './App.css';
-// Это корневой компонент
-// Сохраняйте компоненты в папке components
-// Если создаёте универсальный компонет (например кнопка) сохраняйте в папку components/generic
-// Для своих корневых компонентов можно создавать свои подпапки. Например components/products для компонета автомобиля
+import Button from './components/generic/button';
 
+const HeaderPanel = () => { return ( 
+<>
+<div>HeaderPanel
+<Button />
+<Button />
+</div>
+</>
+ )}
+const MenuPanel = () => {return (
+<>
+<div>
+<Logo />
+<Logo />
+<MainMenu />
+</div>
+</>
+)}
+
+const Logo = () => {return ( <div>Logo</div> )}
+const MainMenu = () => {return ( <div>MainMenu</div> )}
+
+const MainPanel = () => {return ( 
+  <>
+  <BreadCrumbs />
+  <PageTitle />
+  <Component />
+  </>
+ )}
+
+const BreadCrumbs  = () => {return ( <div>BreadCrumbs </div> )}
+const PageTitle = () => {return ( <div>PageTitle</div> )}
+const Component = () => {return ( <div>Component</div> )}
 
 function App() {
   return (
     <div className="App">
-
+      <HeaderPanel />
+      <MenuPanel />
+      <MainPanel />
     </div>
   );
 }
---
+++ web-app/src/components/generic/button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Button = () => { return (<button type="button">Test</button>)};
+
+export default Button;

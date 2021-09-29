ed60a350f34efab373f4aadfc625ea542afa90bc
diamrem <marinasupitsa@gmail.com>
2021-09-30 00:53:29 +0300

    fix:css:вorder

--- web-app/src/components/optional_equipment/Optional_equipment.js
+++ web-app/src/components/optional_equipment/Optional_equipment.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable camelcase */
 /* eslint-disable prettier/prettier */
@@ -14,16 +15,50 @@ function Optional_equipment() {
     <div className="col_optional_equipment">Опциональное оборудование</div>
                     
        <ul>
-          <p>При добавлении опционального оборудования, цена автомобиля изменится</p>
-          
-          <dd><input type="checkbox" id="checkbox"  />Кондиционер с ручным управлением</dd>
-          <dd><input type="checkbox" id="checkbox"  />Пакет “Подготовка к эксплуатации”; пластиковые подкрылки (к-т 2 шт); подогреватель топливного</dd>
-          <dd> фильтра; резиновые коврики в кабину; антигравийная пленка - угловая вкладка двери R,L, к-т 2 шт;</dd>
-          <dd><input type="checkbox" id="checkbox"  />Комплект пневмоусиления подвески с компрессором</dd>
-          <dd><input type="checkbox" id="checkbox"  />Программируемый предпусковой отопитель с пультом дистанционного управления</dd>
-          <dd><input type="checkbox" id="checkbox"  />Пакет улучшения обзорности (электрообогрев лобового стекла, датчик уровня омывающей жидкости</dd>
-          <dd><input type="checkbox" id="checkbox"  />Магнитола с громкой связью и кнопками управления на руле</dd>
-          <dd><input type="checkbox" id="checkbox"  />Круиз-контроль и кожаный руль</dd>
+           <p>При добавлении опционального оборудования, цена автомобиля изменится </p>
+            <dd>
+                <label className="container">Кондиционер с ручным управлением
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Пакет “Подготовка к эксплуатации”; пластиковые подкрылки (к-т 2 шт); подогреватель топливного    
+                    фильтра; резиновые коврики в кабину; антигравийная пленка - угловая вкладка двери R,L, к-т 2 шт;
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Комплект пневмоусиления подвески с компрессором
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Программируемый предпусковой отопитель с пультом дистанционного управления
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Пакет улучшения обзорности (электрообогрев лобового стекла, датчик уровня омывающей жидкости
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Магнитола с громкой связью и кнопками управления на руле
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
+            <dd>
+                <label className="container">Круиз-контроль и кожаный руль
+                   <input type="checkbox" />
+                     <span class="checkmark"/>
+                </label>
+            </dd>
       </ul>
   </div>
   </div>

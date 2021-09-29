8554a4c50cf4a3630ca2c987e72450c44528c7da
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-09-29 19:25:54 +0300

    feat: make class TotalCarValue

--- web-app/src/components/total-car-value/Total-car-value.js
+++ web-app/src/components/total-car-value/Total-car-value.js
@@ -8,3 +8,22 @@ import NextButton from "../generic/Next-btn";
 import Price_list_btn from "../generic/Price_list_btn";
 
 // component for calculating the sum of every part of car's configuration;
+export default class TotalCarValue extends React.Component {
+  render() {
+    return (
+      <div className="total-car-value">
+        <div className="label-car-sum">Общая стоимость автомобиля</div>
+        <div className="label-car-sum-byn">270 950 BYN</div>{" "}
+        {/* здесь  будет значение исходя из выбранных позиций и курса НБ */}
+        <div className="label-car-sum-rub">7 950 865 RUB</div>{" "}
+        {/* здесь  будет значение исходя из выбранных позиций в списке */}
+        <div>
+          <Price_list_btn />
+        </div>
+        <div>
+          <NextButton />
+        </div>
+      </div>
+    );
+  }
+}

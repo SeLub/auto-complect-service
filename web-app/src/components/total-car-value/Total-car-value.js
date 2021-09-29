1cdc5a27e70784fe4c789916fda84daae12f4ab0
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-09-29 19:25:10 +0300

    feat: add imports

---
+++ web-app/src/components/total-car-value/Total-car-value.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-pascal-case */
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+import getExRate from "../../functions/currence-ex-rate";
+import NextButton from "../generic/Next-btn";
+import Price_list_btn from "../generic/Price_list_btn";
+
+// component for calculating the sum of every part of car's configuration;

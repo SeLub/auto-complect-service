6564a83bfeb4c54d269c2e14dd59f54975256f37
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-30 14:56:39 +0300

    feat: add css for a

--- web-app/src/components/picking_switch/Picking_switch.js
+++ web-app/src/components/picking_switch/Picking_switch.js
@@ -1,23 +1,33 @@
-// /* eslint-disable jsx-a11y/alt-text */
+// /* eslint-disable react/no-this-in-sfc */
 /* eslint-disable camelcase */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Picking_switch.css";
 
 function Picking_switch() {
+  // const { basicConfiguration, optionalEquipment, vehicleType } = this.props;
   return (
     <div className="App">
       <div className="picking_switch_wrapper">
         <ul className="picking_switch_ul">
-          <a className="picking_switch_a" aria-current="page" href="#">
+          <a
+            className="picking_switch_a"
+            aria-current="page"
+            onСlick="{basicConfiguration}"
+            href="#"
+          >
             Базовая комплектация
           </a>
 
-          <a className="picking_switch_a" href="#">
+          <a
+            className="picking_switch_a"
+            onСlick="{optionalEquipment}"
+            href="#"
+          >
             Опциональное оборудование
           </a>
 
-          <a className="picking_switch_a" href="#">
+          <a className="picking_switch_a" onСlick="{vehicleType}" href="#">
             Тип транспортного средства
           </a>
         </ul>

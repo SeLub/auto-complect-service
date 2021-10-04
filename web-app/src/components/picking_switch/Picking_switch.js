61a1891f37d3bf41969fdb4c7c514eaef7dd640a
Belashab <belashabb@gmail.com>
2021-10-04 17:20:50 +0300

    fix: horrible crash fix

--- web-app/src/components/picking_switch/Picking_switch.js
+++ web-app/src/components/picking_switch/Picking_switch.js
@@ -1,18 +1,19 @@
-/* eslint-disable react/jsx-pascal-case */
-/* eslint-disable prettier/prettier */
+/* eslint-disable */
 // /* eslint-disable react/no-this-in-sfc */
-/* eslint-disable camelcase */  
+/* eslint-disable camelcase */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import "./Picking_switch.css";
 import Optional_equipment from "../optional_equipment/Optional_equipment";
+import "./Picking_switch.css";
 
 function Picking_switch() {
   return (
     <div className="App">
       <div className="tabs">
         <div id="content-1">Содержимое 1...</div>
-        <div id="content-2"><Optional_equipment /></div>
+        <div id="content-2">
+          <Optional_equipment />
+        </div>
         <div id="content-3">Содержимое 3...</div>
 
         <div className="tabs__links">
--- web-app/src/pages/configurator_p1/configurator_p1.js
+++ web-app/src/pages/configurator_p1/configurator_p1.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /* eslint-disable camelcase */
 /* eslint-disable react/jsx-fragments */
 /* eslint-disable react/jsx-pascal-case */
@@ -7,22 +8,22 @@ import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 import Configurator from "../../components/Configurator";
-import Basic_Options from "../../components/Basic_Options";
-import Block_ford_cards from "../../components/Block_ford_cards"
+// import Basic_Options from "../../components/Basic_Options";
+// import Block_ford_cards from "../../components/Block_ford_cards"
 import Picking_switch from "../../components/picking_switch/Picking_switch";
 // import hull_types from "../../components/hull_static/hull_types_specs.json";
 
 export default function configurator_p1() {
-  const history = useHistory();
-  const hull_types = history.location.state.hull_types;
+//   const history = useHistory();
+//   const hull_types = history.location.state.hull_types;
   return (
     <>
       <AppHeader />
       <BreadCrumbs />
       <Configurator />
-      <Block_ford_cards hull_types={hull_types}/>
+      {/* <Block_ford_cards hull_types={hull_types}/> */}
       <Picking_switch />
-      <Basic_Options hull_types={hull_types.base_options}/>
+      {/* <Basic_Options hull_types={hull_types.base_options}/> */}
     </>
   );
 }

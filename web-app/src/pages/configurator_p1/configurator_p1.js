ac91c8f6b565ae4072742bbdc202cd688dac811a
Belashab <belashabb@gmail.com>
2021-10-04 17:52:21 +0300

    fix: dev crash fix

--- web-app/src/pages/configurator_p1/configurator_p1.js
+++ web-app/src/pages/configurator_p1/configurator_p1.js
@@ -2,29 +2,29 @@
 /* eslint-disable react/jsx-fragments */
 /* eslint-disable react/jsx-pascal-case */
 /* eslint-disable import/no-cycle */
-import { useHistory } from "react-router";
+// import { useHistory } from "react-router-dom";
 import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 import Configurator from "../../components/Configurator";
-import Basic_Options from "../../components/Basic_Options";
-import Block_ford_cards from "../../components/Block_ford_cards";
+// import Basic_Options from "../../components/Basic_Options";
+// import Block_ford_cards from "../../components/Block_ford_cards";
 
 import Picking_switch from "../../components/picking_switch/Picking_switch";
 // import hull_types from "../../components/hull_static/hull_types_specs.json";
 
 export default function configurator_p1() {
-  const history = useHistory();
-  const hull_types = history.location.state.hull_types;
+  //   const history = useHistory();
+  //   const hull_types = history.location.state.hull_types;
 
   return (
     <>
       <AppHeader />
       <BreadCrumbs />
       <Configurator />
-      <Block_ford_cards hull_types={hull_types} />
+      {/* <Block_ford_cards hull_types={hull_types} /> */}
       <Picking_switch />
-      <Basic_Options hull_types={hull_types.base_options} />
+      {/* <Basic_Options hull_types={hull_types.base_options} /> */}
     </>
   );
 }

dd3198deb863bd74f30ca6cd8ca349cc3cabf05c
Belashab <belashabb@gmail.com>
2021-10-11 16:41:43 +0300

    feat: base card layout

---
+++ web-app/src/components/hull_card/Hull_card.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+
+import React from "react";
+import "./hull_card.css";
+
+export default function Hull_card(props){
+    <div className="car_card">
+        <div className = "car_type">
+            <p>
+                {props.card_car.car_type}
+            </p>
+        </div>
+        <div className = "car_desc">
+            <p>
+                {props.card_car.car_desc}
+            </p>
+        </div>
+        <div className = "car_choice">
+            <p>
+                Выбор схемы комплектации:
+            </p>
+        </div>
+        {props.card_car.car_layouts.map((car_layouts, index) => {
+            return(
+                <button className="layout_option">
+                    {car_layouts}
+                </button>
+            )
+        })}
+        <div className = "car_img">
+            <img src = {props.card_car.car_type_pic}></img>
+        </div>
+        <div className = "car_cost">
+        </div>
+        <button className="car_repurpose_btn">
+
+        </button>
+    </div>
+};

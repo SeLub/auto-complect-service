122fc703a62e6ead69fbe8fc0eb918cb6d42f0f4
Belashab <belashabb@gmail.com>
2021-10-11 20:23:18 +0300

    feat tmp_p3 + p3

--- web-app/src/components/tmp_components/tmp_p3.js
+++ web-app/src/components/tmp_components/tmp_p3.js
@@ -4,7 +4,7 @@ import React from "react";
 import { useHistory } from "react-router";
 
 
-export default function tmp_p3(){
+export default function Tmp_p3(){
     const history = useHistory();
     function handleClick(hull_types){
       history.push({
--- web-app/src/pages/configurator_p2/configurator_p2.js
+++ web-app/src/pages/configurator_p2/configurator_p2.js
@@ -1,9 +1,12 @@
 /* eslint-disable */
 
 import React from "react";
+import AppHeader from "../../components/app-header";
 
 export default function configurator_p2(){
+    return(
     <>
         <AppHeader />
     </>
+    )
 }

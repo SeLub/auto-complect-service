1b32cb68e55b7fffda9c4a2d0fe20e70f9bf96af
Belashab <belashabb@gmail.com>
2021-10-11 14:38:51 +0300

    feat block block_ford_card_page_2 hardcode

--- web-app/src/pages/configurator_p1/configurator_p1.js
+++ web-app/src/pages/configurator_p1/configurator_p1.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-fragments */
 /* eslint-disable react/jsx-pascal-case */
 /* eslint-disable import/no-cycle */
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
@@ -14,13 +14,19 @@ import Picking_switch from "../../components/picking_switch/Picking_switch";
 // import hull_types from "../../components/hull_static/hull_types_specs.json";
 
 export default function configurator_p1() {
-  const history = useHistory();
+  // const history = useHistory();
+  const pictires = {
+    pic1: "/img/main_pic5.jpg",
+    pic2: "/img/main_pic501.jpg",
+    pic3: "/img/main_pic502.jpg",
+    pic4: "/img/main_pic503.jpg",
+  };
   return (
     <>
       <AppHeader />
       <BreadCrumbs />
       <Configurator />
-      <Block_ford_cards hull_types={history.location.state.hull_types} />
+      <Block_ford_cards hull_types={pictires} />
       <Picking_switch />
       {/* <Basic_Options hull_types={hull_types.base_options} /> */}
     </>

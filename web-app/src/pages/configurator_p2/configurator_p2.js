2b05e76b535193831d752ec275b3f94f260eb166
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-13 18:56:38 +0300

    feat: add component to page 2

--- web-app/src/pages/configurator_p2/configurator_p2.js
+++ web-app/src/pages/configurator_p2/configurator_p2.js
@@ -5,11 +5,13 @@ import AppHeader from "../../components/app-header";
 import ReEquipment from "../../components/ReEquipment";
 import RefitTabs from "../../components/refit-tabs/RefitTabs";
 import ReEquipmentText from "../../components/re-equipment-text/ReEquipmentText";
+import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 
 export default function configurator_p2() {
   return (
     <>
       <AppHeader />
+      <BreadCrumbs />
       <ReEquipment />
       <ReEquipmentText />
       <RefitTabs />

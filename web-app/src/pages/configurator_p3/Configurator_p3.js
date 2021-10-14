18ef9e9920ec731b92731019d5215f6bd6456291
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-14 22:47:49 +0300

    feat: make third page

--- web-app/src/pages/configurator_p3/Configurator_p3.js
+++ web-app/src/pages/configurator_p3/Configurator_p3.js
@@ -1,3 +1,17 @@
 import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
+
+/* cюда добавляйте imports  своих компонентов */
+
+const ConfiguratorP3 = () => {
+  return (
+    <div>
+      <AppHeader />
+      <BreadCrumbs />
+      {/* cюда добавляйте свои компоненты */}
+    </div>
+  );
+};
+
+export default ConfiguratorP3;

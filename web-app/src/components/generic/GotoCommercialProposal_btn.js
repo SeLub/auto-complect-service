990db11c83ec7f35e5b3852a0b6349ffd430ca54
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-14 23:09:45 +0300

    fix: fix history.push location of third page

--- web-app/src/components/generic/GotoCommercialProposal_btn.js
+++ web-app/src/components/generic/GotoCommercialProposal_btn.js
@@ -1,12 +1,12 @@
-import ConfiguratorP3 from "../../pages/configurator_p3/Configurator_p3";
 import React from "react";
 import { useHistory } from "react-router-dom";
+import ConfiguratorP3 from "../../pages/configurator_p3/Configurator_p3";
 
 const GotoCommercialProposalBtn = () => {
   const history = useHistory();
 
   function handleClick() {
-    history.push("/web-app/src/pages/configurator_p3");
+    history.push(ConfiguratorP3);
   }
   return (
     <div className="gotoCom">

3cde59dfcafe3fbcabd6aebf655fad55d9f790a6
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-14 23:05:18 +0300

    feat: add react-router  for going to 3 page

---
+++ web-app/src/components/generic/GotoCommercialProposal_btn.js
@@ -0,0 +1,20 @@
+import ConfiguratorP3 from "../../pages/configurator_p3/Configurator_p3";
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const GotoCommercialProposalBtn = () => {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/web-app/src/pages/configurator_p3");
+  }
+  return (
+    <div className="gotoCom">
+      <button type="button" onClick={handleClick}>
+        Перейти к оформлению КП
+      </button>
+    </div>
+  );
+};
+
+export default GotoCommercialProposalBtn;

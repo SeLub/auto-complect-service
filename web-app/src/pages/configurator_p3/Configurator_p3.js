06a7fb35084b267ec59a9d9e207b1a6600e69ee3
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-15 00:48:33 +0300

    fix: fix function name

--- web-app/src/pages/configurator_p3/Configurator_p3.js
+++ web-app/src/pages/configurator_p3/Configurator_p3.js
@@ -1,17 +1,17 @@
+/* eslint-disable react/jsx-fragments */
+/* eslint-disable camelcase */
 import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 
 /* cюда добавляйте imports  своих компонентов */
 
-const ConfiguratorP3 = () => {
+export default function Configurator_p3() {
   return (
-    <div>
+    <>
       <AppHeader />
       <BreadCrumbs />
       {/* cюда добавляйте свои компоненты */}
-    </div>
+    </>
   );
-};
-
-export default ConfiguratorP3;
+}

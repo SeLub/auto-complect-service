77c4c2a078e0aac6a3a7afbd871e67080b10ed1f
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-17 21:34:28 +0300

    feat: add function which is read json and return menu items

--- web-app/src/components/managerContactDetails/managerContactDetails.js
+++ web-app/src/components/managerContactDetails/managerContactDetails.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-console */
 import * as React from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import managerContactDetails from "./baseManagers.json";
+
+/* Компонент Box служит в качестве компонента-оболочки. */
+
+/* FormControl Предоставляет контекст, такой как заполненный
+    сфокусированный, ошибка,необходимый для ввода формы В FormControl можно
+    использовать только одну InputBase */
 
 export default function ManagerContactDetails() {
   const [managerName, setManagerName] = React.useState(" ");
@@ -16,4 +24,22 @@ export default function ManagerContactDetails() {
     event.preventDefault();
     setManagerName(event.target.value);
   };
+
+  return (
+    <Box sx={{ height: 441, wight: 455 }}>
+      <FormControl fullWidth>
+        <InputLabel id="select-label-manager">Выберите менеджера</InputLabel>
+        <Select
+          labelId="select-label-manager"
+          id="select-manager"
+          value={managerName}
+          onChange={handleChange}
+        >
+          {managerContactDetails.map((element) => {
+            return <MenuItem value={element.id}>{element.name}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }

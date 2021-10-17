8d2926691a4863392c668a1f57c7b0dfbbee0320
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-17 12:40:00 +0300

    feat: add handleChange function

--- web-app/src/components/managerContactDetails/managerContactDetails.js
+++ web-app/src/components/managerContactDetails/managerContactDetails.js
@@ -4,3 +4,16 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+
+export default function ManagerContactDetails() {
+  const [managerName, setManagerName] = React.useState(" ");
+
+  // функция которая считывает выбранное значение
+
+  const handleChange = (event) => {
+    // не обновляет всю страницу при изменении значения в выпадающем списке
+
+    event.preventDefault();
+    setManagerName(event.target.value);
+  };
+}

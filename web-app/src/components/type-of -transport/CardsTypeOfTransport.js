e7cf6f8a18b63445bd1900471754801cfe39eb4a
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-21 23:03:04 +0300

    feat: add imports

---
+++ web-app/src/components/type-of -transport/CardsTypeOfTransport.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { Card } from "@mui/material";
+import { CardActions } from "@mui/material";
+import { CardContent } from "@mui/material";
+import { CardMedia } from "@mui/material";
+import { CardHeader } from "@mui/material";
+import { Typography } from "@mui/material";

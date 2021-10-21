8376e3b51bed965cab1fa8c80be3999aafd55638
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-22 00:11:07 +0300

    feat: add Typography Выбор схемы комплектации:

--- web-app/src/components/type-of -transport/CardsCargoPassenger.js
+++ web-app/src/components/type-of -transport/CardsCargoPassenger.js
@@ -17,6 +17,9 @@ export default function CardsCargoPassenger() {
       <Typography variant="body2" color="text.secondary">
         Грузопассажирский автомобиль 5+1 на базе L2H2
       </Typography>
+      <Typography variant="body2" color="InfoText">
+        Выбор схемы комплектации:
+      </Typography>
     </CardContent>
   );
 }

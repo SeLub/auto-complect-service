4411b3ab9f6ebfb17b5ea5e7cc7474567cb04006
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-22 09:51:15 +0300

    feat: add Card mui

--- web-app/src/components/type-of -transport/CardsCargoPassenger.js
+++ web-app/src/components/type-of -transport/CardsCargoPassenger.js
@@ -12,19 +12,21 @@ import TotalCarValue from "../total-car-value/Total-car-value";
 
 export default function CardsCargoPassenger() {
   return (
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        Грузопассажирский
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        Грузопассажирский автомобиль 5+1 на базе L2H2
-      </Typography>
-      <Typography variant="body2" color="InfoText">
-        Выбор схемы комплектации:
-      </Typography>
-      <Box>
-        <TotalCarValue />
-      </Box>
-    </CardContent>
+    <Card sx={{ minWidth: 360 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Грузопассажирский
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Грузопассажирский автомобиль 5+1 на базе L2H2
+        </Typography>
+        <Typography variant="body2" color="InfoText">
+          Выбор схемы комплектации:
+        </Typography>
+        <Box>
+          <TotalCarValue />
+        </Box>
+      </CardContent>
+    </Card>
   );
 }

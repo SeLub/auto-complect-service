00019e82b857d20edf134c65bba332d73b0c8edf
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-10-22 14:11:49 +0300

    feat: add ToggleButtons for cargo

---
+++ web-app/src/components/type-of -transport/ToggleButtonCargo.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+export default function ToggleButtonCargo() {
+  const [alignment, setAlignment] = React.useState("5/6+1");
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+    >
+      <ToggleButton value="5/6+1">Web</ToggleButton>
+      <ToggleButton value="5/6+1">Android</ToggleButton>
+      <ToggleButton value="5/6/8+1">iOS</ToggleButton>
+    </ToggleButtonGroup>
+  );
+}

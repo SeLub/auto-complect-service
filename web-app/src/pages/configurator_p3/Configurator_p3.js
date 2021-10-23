01b3028cd9216d3b525f35dea0965bc6b6faa17b
diamrem <marinasupitsa@gmail.com>
2021-10-24 00:28:30 +0300

    feat:add new names of files

--- web-app/src/pages/configurator_p3/Configurator_p3.js
+++ web-app/src/pages/configurator_p3/Configurator_p3.js
@@ -3,8 +3,8 @@
 import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
-import TabRequestExecution3 from "../../components/Tab-request-execution/TabRequestExecution3";
-import TabRequestExecution4 from "../../components/Tab-request-execution/TabRequestExecution4";
+// import TabRequestExecution3 from "../../components/Tab-request-execution/TabRequestExecution3";
+// import TabRequestExecution4 from "../../components/Tab-request-execution/TabRequestExecution4";
 /* cюда добавляйте imports  своих компонентов */
 
 export default function Configurator_p3() {
@@ -12,8 +12,8 @@ export default function Configurator_p3() {
     <>
       <AppHeader />
       <BreadCrumbs />
-      <TabRequestExecution3 />
-      <TabRequestExecution4 />
+      {/* <TabRequestExecution3 />
+      <TabRequestExecution4 /> */}
     </>
   );
 }

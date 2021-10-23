63f84f742de664c744e64eb608dfeb6ed0fedf0d
diamrem <marinasupitsa@gmail.com>
2021-10-24 00:14:38 +0300

    feat:chage tables

--- web-app/src/pages/configurator_p3/Configurator_p3.js
+++ web-app/src/pages/configurator_p3/Configurator_p3.js
@@ -4,6 +4,7 @@ import React from "react";
 import AppHeader from "../../components/app-header";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 import TabRequestExecution3 from "../../components/Tab-request-execution/TabRequestExecution3";
+import TabRequestExecution4 from "../../components/Tab-request-execution/TabRequestExecution4";
 /* cюда добавляйте imports  своих компонентов */
 
 export default function Configurator_p3() {
@@ -12,6 +13,7 @@ export default function Configurator_p3() {
       <AppHeader />
       <BreadCrumbs />
       <TabRequestExecution3 />
+      <TabRequestExecution4 />
     </>
   );
 }

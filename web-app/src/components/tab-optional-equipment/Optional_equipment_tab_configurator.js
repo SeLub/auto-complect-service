b8c11a355c899118effe4a09b7c7e7481ea76966
diamrem <marinasupitsa@gmail.com>
2021-10-25 14:10:06 +0300

    feat:num of pages

--- web-app/src/components/tab-optional-equipment/Optional_equipment_tab_configurator.js
+++ web-app/src/components/tab-optional-equipment/Optional_equipment_tab_configurator.js
@@ -29,8 +29,8 @@ export default function DataTable() {
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={8}
-        rowsPerPageOptions={[8]}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
         checkboxSelection
       />
     </div>

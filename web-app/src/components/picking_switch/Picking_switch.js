2c01d76f0211af93087412a16f2e0187df4d86de
Belashab <belashabb@gmail.com>
2021-10-26 13:32:26 +0300

    fix: final fix for dev merge

--- web-app/src/components/picking_switch/Picking_switch.js
+++ web-app/src/components/picking_switch/Picking_switch.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 /* eslint-disable */
 /* eslint-disable react/no-this-in-sfc */
 /* eslint-disable camelcase */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-=======
->>>>>>> origin/main
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from "react";
 import PropTypes from "prop-types";
@@ -15,7 +12,6 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-<<<<<<< HEAD
 import Basic_Options from "../../components/basic_options/Basic_Options";
 import Optional_equipment_tab_configurator from "../tab-optional-equipment/Optional_equipment_tab_configurator";
 import СardLayoutTypeTransport from "../type-of -transport/СardLayoutTypeTransport";
@@ -23,11 +19,6 @@ import TotalCarValue from "../total-car-value/Total-car-value";
 import GotoCommercialProposalBtn from "../generic/GotoCommercialProposal_btn";
 
 function RefitTabs(props) {
-=======
-import "./Picking_switch.css";
-
-function TabPanel(props) {
->>>>>>> origin/main
   const { children, value, index, ...other } = props;
 
   return (
@@ -44,20 +35,12 @@ function TabPanel(props) {
   );
 }
 
-<<<<<<< HEAD
 RefitTabs.propTypes = {
-=======
-TabPanel.propTypes = {
->>>>>>> origin/main
   children: PropTypes.node,
   index: PropTypes.number.isRequired,
   value: PropTypes.number.isRequired,
 };
-<<<<<<< HEAD
 RefitTabs.defaultProps = {
-=======
-TabPanel.defaultProps = {
->>>>>>> origin/main
   children: "",
 };
 
@@ -67,7 +50,6 @@ function a11yProps(index) {
     "aria-controls": `action-tabpanel-${index}`,
   };
 }
-<<<<<<< HEAD
 const css = `
 @import url("http://fonts.cdnfonts.com/css/ford-antenna");
   .box {
@@ -121,8 +103,6 @@ const css = `
 .css-19kzrtu {
   padding: 24px 0px;
 }`;
-=======
->>>>>>> origin/main
 
 export default function FloatingActionButtonZoom() {
   const theme = useTheme();
@@ -137,7 +117,6 @@ export default function FloatingActionButtonZoom() {
   };
   return (
     <div className="App">
-<<<<<<< HEAD
       <style type="text/css">{css}</style>
       <Box className="box">
         <AppBar className="appbar" position="static">
@@ -149,62 +128,27 @@ export default function FloatingActionButtonZoom() {
           >
             <Tab
               className="tab1"
-=======
-      <Box
-        className="box"
-        sx={{
-          bgcolor: "background.paper",
-          width: 1128,
-          position: "relative",
-          minHeight: 200,
-        }}
-      >
-        <AppBar position="static" color="default">
-          <Tabs
-            className="tabs"
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="fullWidth"
-            aria-label="action tabs example"
-          >
-            <Tab
-              className="tab"
->>>>>>> origin/main
               label="Базовая комплектация"
               {...a11yProps(0)}
             />
             <Tab
-<<<<<<< HEAD
               className="tab2"
-=======
-              className="tab"
->>>>>>> origin/main
               label="Опциональное оборудование"
               {...a11yProps(1)}
             />
             <Tab
-<<<<<<< HEAD
               className="tab3"
-=======
-              className="tab"
->>>>>>> origin/main
               label="Тип транспортного средства"
               {...a11yProps(2)}
             />
           </Tabs>
         </AppBar>
         <SwipeableViews
-<<<<<<< HEAD
           className="swipeableviews"
-=======
->>>>>>> origin/main
           axis={theme.direction === "rtl" ? "x-reverse" : "x"}
           index={value}
           onChangeIndex={handleChangeIndex}
         >
-<<<<<<< HEAD
           <RefitTabs value={value} index={0} dir={theme.direction}>
             <Basic_Options />
           </RefitTabs>
@@ -216,17 +160,6 @@ export default function FloatingActionButtonZoom() {
             <TotalCarValue />
             <GotoCommercialProposalBtn />
           </RefitTabs>
-=======
-          <TabPanel value={value} index={0} dir={theme.direction}>
-            Базовая комплектация(текст)
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            Опциональное оборудование(текст)
-          </TabPanel>
-          <TabPanel value={value} index={2} dir={theme.direction}>
-            Тип транспортного средства
-          </TabPanel>
->>>>>>> origin/main
         </SwipeableViews>
       </Box>
     </div>

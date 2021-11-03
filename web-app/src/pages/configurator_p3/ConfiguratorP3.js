421d4c291dc80ce3b460658a1ccf045a19d6770e
diamrem <marinasupitsa@gmail.com>
2021-11-03 16:03:29 +0300

    feat:error correction

--- web-app/src/pages/configurator_p3/ConfiguratorP3.js
+++ web-app/src/pages/configurator_p3/ConfiguratorP3.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD:web-app/src/pages/configurator_p3/Configurator_p3.js
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/jsx-fragments */
 /* eslint-disable camelcase */
-=======
->>>>>>> origin/dev:web-app/src/pages/configurator_p3/ConfiguratorP3.js
 import React from "react";
 import AppHeader from "../../components/app-header/AppHeader";
 import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
@@ -10,16 +8,6 @@ import BreadCrumbs from "../../components/breadcrumb/Breadcrumb";
 // import TabRequestExecution4 from "../../components/Tab-request-execution/TabRequestExecution4";
 /* cюда добавляйте imports  своих компонентов */
 
-<<<<<<< HEAD:web-app/src/pages/configurator_p3/Configurator_p3.js
-export default function Configurator_p3() {
-  return (
-    <>
-      <AppHeader />
-      <BreadCrumbs />
-      {/* <TabRequestExecution3 />
-      <TabRequestExecution4 /> */}
-    </>
-=======
 const css = `
   .flex {
     display: flex;
@@ -39,6 +27,5 @@ export default function ConfiguratorP3() {
 
       <RequestExecutionTabs />
     </div>
->>>>>>> origin/dev:web-app/src/pages/configurator_p3/ConfiguratorP3.js
   );
 }

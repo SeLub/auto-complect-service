054c8b000ff1f16cdcf7a58dd3640de6a2687ab3
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-11-09 18:40:16 +0300

    feat: add toggleBtnBus reducer

--- web-app/src/reducers/reducers.js
+++ web-app/src/reducers/reducers.js
@@ -2,12 +2,14 @@ import hullTypes from "./hull_types_reducer";
 import colorSetter from "./colorSetter";
 import toggleBtnAutomobile from "./ToggleButtonAutomobile_reducer";
 import priceTracker from "./priceTracker";
+import toggleBtnBus from "./ToggleButtonBus_reducer";
 
 const reducers = {
   hullTypes,
   colorSetter,
   toggleBtnAutomobile,
   priceTracker,
+  toggleBtnBus,
 };
 
 export default reducers;

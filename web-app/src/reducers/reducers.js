88e5d2d9545905bf3cdc4d660acfc9e24e2d9adf
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-11-09 13:33:19 +0300

    feat: add toggleBtnAutomobile reducer

--- web-app/src/reducers/reducers.js
+++ web-app/src/reducers/reducers.js
@@ -1,11 +1,13 @@
 import hullTypes from "./hull_types_reducer";
 import counter from "./conter";
 import colorSetter from "./colorSetter";
+import toggleBtnAutomobile from "./ToggleButtonAutomobile_reducer";
 
 const reducers = {
   hullTypes,
   counter,
   colorSetter,
+  toggleBtnAutomobile,
 };
 
 export default reducers;

b616de22905258e2f8042ca5a222d3ffdd3f96f8
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-11-09 16:33:21 +0300

    feat: add dispatch and selector for redux issues

--- web-app/src/components/type-of -transport/ToggleButtonAutomobile.js
+++ web-app/src/components/type-of -transport/ToggleButtonAutomobile.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { styled } from "@mui/material/styles";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ToggleButtonAutomobile() {
-  const [alignment, setAlignment] = React.useState("8+1");
+  const dispatch = useDispatch();
+  const alignment = useSelector((state) => state.toggleBtnAutomobile.alignment);
 
-  const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
+  const handleChange = () => {
+    dispatch({ type: "8+1", payload: "8+1" });
   };
 
   const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({

4585aa77dfd95793c9bb513148334faf095f7a94
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-11-09 21:58:52 +0300

    feat: add reducers

--- web-app/src/reducers/reducers.js
+++ web-app/src/reducers/reducers.js
@@ -1,15 +1,17 @@
 import hullTypes from "./hull_types_reducer";
 import colorSetter from "./colorSetter";
-import toggleBtnAutomobile from "./ToggleButtonAutomobile_reducer";
+import toggleBtnAutomobile from "./toggleButtonAutomobile_reducer";
 import priceTracker from "./priceTracker";
-import toggleBtnBus from "./ToggleButtonBus_reducer";
+import toggleBtnBus from "./toggleButtonBus_reducer";
+import toggleBtnCargo from "./toggleButtonCargo_reducer";
 
 const reducers = {
   hullTypes,
   colorSetter,
-  toggleBtnAutomobile,
   priceTracker,
   toggleBtnBus,
+  toggleBtnCargo,
+  toggleBtnAutomobile,
 };
 
 export default reducers;

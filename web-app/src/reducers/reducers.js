2fa2d934a18ffe7d1756f783e377fdbdc9b378f7
Belashab <belashabb@gmail.com>
2021-11-09 14:22:45 +0300

    remove unused counter reducer

--- web-app/src/reducers/reducers.js
+++ web-app/src/reducers/reducers.js
@@ -1,10 +1,8 @@
 import hullTypes from "./hull_types_reducer";
-import counter from "./conter";
 import colorSetter from "./colorSetter";
 
 const reducers = {
   hullTypes,
-  counter,
   colorSetter,
 };
 

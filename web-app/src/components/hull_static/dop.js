922f977d93f7b4da704cc6736a4590be3cb8db1f
diamrem <marinasupitsa@gmail.com>
2021-11-11 19:08:48 +0300

    feat:changed copper options

---
+++ web-app/src/components/hull_static/dop.js
@@ -0,0 +1,32 @@
+// {
+//     "option_name": "Защита колесных арок  (комплект 4 шт.)",
+//     "option_cost": 17540,
+//     "option_cost_currency": "RUB"
+// },
+// {
+//     "option_name": "Защита ДВС стальная",
+//     "option_cost": 6580,
+//     "option_cost_currency": "RUB"
+// },
+// {
+//     "option_name": "Коврики салона передние резиновые",
+//     "option_cost": 2000,
+//     "option_cost_currency": "RUB"
+// }
+
+// {
+//     "option_name": "Усиленная передняя подвеска",
+//     "option_cost": 12500,
+//     "option_cost_currency": "RUB"
+// },
+// {
+//     "option_name": "Усиленная передняя подвеска",
+//     "option_cost": 12500,
+//     "option_cost_currency": "RUB"
+// },
+
+// {
+//     "option_name": "Сигнализация периметра (при наличии предпускового отопителя)",
+//     "option_cost": 14500
+//     "option_cost_currency": "RUB"
+// },
